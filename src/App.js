2abcb679d2f361017e4de1ba56f9baa974b7fb60
INZEMAM SADIQUE <nargistarannum1@gmail.com>
2023-12-15 17:04:06 +0530

    working to design authentication form

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
+import React , {useState,useEffect} from 'react'
 import './App.css';
 
 function App() {
+  const [username,setUserName] = useState('')
+  const [password,setPassword] = useState('')
+
+  useEffect = () => {
+    fetch('https://dummyjson.com/auth/login', {
+  method: 'POST',
+  headers: { 'Content-Type': 'application/json' },
+  body: JSON.stringify({
+    
+    username: 'kminchelle',
+    password: '0lelplR',
+  })
+})
+.then(res => res.json())
+.then(setUserName(res.json()));
+  }
+  
+const submitForm = () => {
+  console.log("ppppppppp");
+}
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          User Registration Formgit 
-        </a>
-      </header>
+         <div className='formDesign'>
+         <form className='form' onSubmit={submitForm}>
+         <p>User Registration Form</p>
+          <div className= 'input-container'>
+            <label>User Name</label>
+            <input type =  "text" value={username} onChange={(e) => setUserName(e.target.value)} placeholder='Enter User Name'/>
+          </div>
+          <div className= 'input-container'>
+            <label>Password</label>
+            <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder='Enter valid Password'/>
+          </div>
+          <div>
+            <input type="submit" />
+          </div>
+         </form>
+      
+         </div>
     </div>
   );
 }
